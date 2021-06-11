eb1a85fe41a46f0d9d8fac5e1eeaaf8a7efed803
Juan Esteban <jedp082@gmail.com>
2021-06-11 15:30:53 -0500

    Component idea category card done! 💡

---
+++ src/components/ideas/categoryCard/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './styles.scss'
+
+interface IdeaCategoryCardProps {
+    title: string,
+    abstract: string,
+    color?: 'purple' | 'main' | 'light-blue',
+    link: string
+}
+
+export const IdeaCategoryCard:React.FC<IdeaCategoryCardProps> = ({ title, abstract, color = "main", link }) => {
+
+    return (
+        <a href={link} className={`flex flex-col align-start m-md justify-center idea-category-card idea-category-card--color-${color}`}>
+            <h2 className="font-sans font-bold">
+                {title}
+            </h2>
+            <p className="font-sans">
+                {abstract}
+            </p>
+        </a>
+    )
+}
