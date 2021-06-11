9a621e1a7d9b6ddcb83360c52e157fa766fa7700
Juan Esteban <jedp082@gmail.com>
2021-06-10 23:01:21 -0500

    Process Card almost done, left cool animation and tests

---
+++ src/components/process/card/index.tsx
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react'
+import { Content } from '../../content'
+import './styles.scss'
+
+export interface ProcessCardProps {
+    title: string,
+    abstract: string,
+    description: string,
+    purposes: string[],
+    color?: 'white' | 'black',
+    showDetail?: boolean,
+    onAction?: () => void
+}
+
+export const ProcessCard:React.FC<ProcessCardProps> = ({ title, description, abstract, purposes, color = 'white', showDetail = false, onAction = () => {} }) => {
+
+    const [show, setShow] = useState<boolean>(false)
+
+    
+    return (
+        <article className={`process-card process-card--color-${color} ${show ? 'process-card--detail' : ''}`} onClick={() => setShow(!show)}>
+            <Content
+                color={color}
+            >
+                <h3 className="font-sans"> {title} </h3>
+                {
+                    !show ? (
+                        <p className="font-sans p-b-md p-t-md"> {abstract} </p>
+                    ) : (
+                        <p className="font-sans p-b-md p-t-md process-card__description"> {description} </p>
+                    )
+                }
+                {
+                    show && (
+                        <div className="process-card__purposes">
+                            <h4 className="font-sans p-t-md">Purposes</h4>
+                            <ul className="process-card__list">
+                               {
+                                   purposes.map((purpose, i) => (
+                                       <li key={i} className="font-sans font-thin"><span className="process-card__list-circle" />{purpose}</li>
+                                   ))
+                               } 
+                            </ul>
+                        </div>
+                    )
+                }
+            </Content>
+            <span className={`text-size-base-md font-sans font-bold color-${color == 'white' ? 'secondary' : 'primary'}`}>{show ? 'See Less' : 'See More'}</span>
+        </article>
+    )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { BasicLayout } from '../layouts/basic'
 
-import { Content } from '../components/content'
+import { ProcessCard } from '../components/process/card'
 
 import { GeneralContext } from '../context/generalContext'
 
@@ -10,18 +10,21 @@ export const App:React.FC = () => {
 
     const { handleTheme } = useContext(GeneralContext)
 
+    const [show, setShow] = useState<boolean>(false)
+
     return (
         <Router>
             <Switch>
                 <Route path="/" component={() => (
                     <BasicLayout>
-                        <Content
-                            color="white"
-                            size="full"
-                            position="center"
-                        >
-                            
-                        </Content>
+                        <ProcessCard 
+                            title="Storytelling"
+                            abstract="Stories for change life"
+                            description="Stories are the best way to chagne the life of everybody, this is text to create a longer description and see the result that can happen just a test, so don't worry, so there is more ever more text just for tests and texts and see the result so I will start copy around shit to see what is going on "
+                            purposes={['branding', 'ux', 'ui']}
+                            showDetail={show}
+                            onAction={handleTheme}
+                        />
                     </BasicLayout>)} />
             </Switch>
         </Router>
